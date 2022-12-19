08334744faba41b02ab165e74d853bd58949d453
elliotghr <gunsnroses.2617@gmail.com>
2022-12-19 11:14:54 -0600

    Envío con form submit

--- react-ejercicios/src/components/ContactForm.js
+++ react-ejercicios/src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import { useForm } from "../hooks/useForm";
-
+import Message from "./Message";
+import Loader from "./Loader";
 const initialForm = {
   name: "",
   email: "",
@@ -9,6 +10,7 @@ const initialForm = {
 
 const validationsForm = (form) => {
   let errors = {};
+
   let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
   let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
   let regexComments = /^.{1,255}$/;
@@ -53,52 +55,60 @@ const ContactForm = () => {
   return (
     <div>
       <h2>Fromulario de contacto</h2>
-      <form onSubmit={handleSubmit}></form>
-      <input
-        type="text"
-        name="name"
-        placeholder="Escribe tu nombre"
-        // Validación cuando se pierda el foco del input
-        onBlur={handleBlur}
-        onChange={handleChange}
-        value={form.name}
-        required
-      ></input>
-      {errors.name && <p style={styles}>{errors.name}</p>}
-      <input
-        type="email"
-        name="email"
-        placeholder="Escribe tu email"
-        // Validación cuando se pierda el foco del input
-        onBlur={handleBlur}
-        onChange={handleChange}
-        value={form.email}
-        required
-      ></input>
-      {errors.email && <p style={styles}>{errors.email}</p>}
-      <input
-        type="text"
-        name="subject"
-        placeholder="Asunto a tratar"
-        // Validación cuando se pierda el foco del input
-        onBlur={handleBlur}
-        onChange={handleChange}
-        value={form.subject}
-        required
-      ></input>
-      {errors.subject && <p style={styles}>{errors.subject}</p>}
-      <textarea
-        name="comments"
-        cols="50"
-        rows="5"
-        placeholder="Escribe tus comentarios"
-        onBlur={handleBlur}
-        onChange={handleChange}
-        value={form.comments}
-        required
-      ></textarea>
-      {errors.comments && <p style={styles}>{errors.comments}</p>}
-      <input type="submit" value="Enviar"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Escribe tu nombre"
+          // Validación cuando se pierda el foco del input
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={form.name}
+          required
+        ></input>
+        {errors.name && <p style={styles}>{errors.name}</p>}
+        <input
+          type="email"
+          name="email"
+          placeholder="Escribe tu email"
+          // Validación cuando se pierda el foco del input
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={form.email}
+          required
+        ></input>
+        {errors.email && <p style={styles}>{errors.email}</p>}
+        <input
+          type="text"
+          name="subject"
+          placeholder="Asunto a tratar"
+          // Validación cuando se pierda el foco del input
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={form.subject}
+          required
+        ></input>
+        {errors.subject && <p style={styles}>{errors.subject}</p>}
+        <textarea
+          name="comments"
+          cols="50"
+          rows="5"
+          placeholder="Escribe tus comentarios"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={form.comments}
+          required
+        ></textarea>
+        {errors.comments && <p style={styles}>{errors.comments}</p>}
+        <input type="submit" value="Enviar"></input>
+        {loading && <Loader></Loader>}
+        {response && (
+          <Message
+            msg="Los datos han sido enviados correctamente"
+            bgColor="green"
+          ></Message>
+        )}
+      </form>
     </div>
   );
 };
--- react-ejercicios/src/hooks/useForm.js
+++ react-ejercicios/src/hooks/useForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 // validateForm función para nuestro blur
 export const useForm = (initialForm, validateForm) => {
   const [form, setForm] = useState(initialForm);
@@ -19,7 +20,33 @@ export const useForm = (initialForm, validateForm) => {
     handleChange(e);
     setErrors(validateForm(form));
   };
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Evaluamos que no existan errores
+    setErrors(validateForm(form));
+    if (Object.keys(errors).length === 0) {
+      // alert("Enviando formulario");
+      setLoading(true);
+      helpHttp()
+        .post("https://formsubmit.co/ajax/gunsnroses.2618@gmail.com", {
+          body: form,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        })
+        .then((res) => {
+          setLoading(false);
+          setResponse(true);
+          setForm(initialForm);
+          setTimeout(() => {
+            setResponse(false);
+          }, 3000);
+        });
+    } else {
+      return;
+    }
+  };
   return {
     form,
     errors,
